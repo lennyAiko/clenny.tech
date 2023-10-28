14fbdee526ae8ee28b539aac0066b8d8caedbe12
lennox charles <lennyaiko17@gmail.com>
2023-10-28 14:28:13 +0100

    feat: setup header and images

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file
+// You can freely import this from anywhere in your project
+
+export const SITE_TITLE = 'Lennox C Charles'
+export const SITE_DESCRIPTION = 'Indie hacker, Web developer.'
