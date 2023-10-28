9a497d7d109f09344df369e37202384e4381ad15
lennox charles <lennyaiko17@gmail.com>
2023-10-28 16:38:27 +0100

    feat: implement nav & links, add prettier & husky

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				purple: {
+					DEFAULT: '#302B3D',
+					50: '#877DA3',
+					100: '#7C719B',
+					200: '#685E85',
+					300: '#564D6D',
+					400: '#433C55',
+					500: '#302B3D',
+					600: '#16141C',
+					700: '#000000',
+					800: '#000000',
+					900: '#000000',
+				}
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
 }
