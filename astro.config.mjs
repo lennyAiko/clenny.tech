3f3726202c7a28c1cea5fbd8511dd55788cda673
lennyaiko <lennyaiko17@gmail.com>
2025-02-14 14:19:45 +0100

    chore

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://clenny.tech",
+  site: "https://clenny.netlify.app",
   integrations: [tailwind(), mdx(), sitemap()],
 });
